0cd5ba356425246505e3ae6c747f6124cbcbe3c3
petrixro <petrix211@gmail.com>
2021-03-24 14:21:15 +0200

    deployed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
 import axios from "axios";
 
